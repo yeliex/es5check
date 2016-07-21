4db07ad7af88aba83a6173c001dec03337334735
yeliex <yeliex@yeliex.com>
2016-07-21 12:44:53 +0800

    Fix alert when use class in style

--- es5check.config.js
+++ es5check.config.js
@@ -8,7 +8,7 @@ module.exports ={
   "rules": {
     "no-arrow-functions": /=>/g,
     'no-for-of': /for(.*)of/g,
-    'no-class': /class/g,
+    'no-class': /class /g,
     'no-template-string': /`/g,
     'no-spread': /\.\.\./g,
     'no-let-or-const': /let|const/g,
